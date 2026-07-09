10a7d76cf2292f2bd7003f88c3e1611a77c28aa2
Your Name <ramljak@windows>
2026-07-09 19:37:50 +0200

    Make in-memory repository thread safe

--- src/HotelSearch.Infrastructure/Repositories/InMemoryHotelRepository.cs
+++ src/HotelSearch.Infrastructure/Repositories/InMemoryHotelRepository.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using HotelSearch.Application.Abstractions;
 using HotelSearch.Domain.Entities;
 
@@ -5,44 +6,37 @@ namespace HotelSearch.Infrastructure.Repositories;
 
 public sealed class InMemoryHotelRepository : IHotelRepository
 {
-    private readonly List<Hotel> _hotels = [];
+    private readonly ConcurrentDictionary<Guid, Hotel> _hotels = new();
 
     public Task<Hotel> AddAsync(Hotel hotel)
     {
-        _hotels.Add(hotel);
+        _hotels[hotel.Id] = hotel;
         return Task.FromResult(hotel);
     }
 
     public Task<Hotel?> GetByIdAsync(Guid id)
     {
-        Hotel? hotel = _hotels.FirstOrDefault(hotel => hotel.Id == id);
+        _hotels.TryGetValue(id, out Hotel? hotel);
         return Task.FromResult(hotel);
     }
 
     public Task<IReadOnlyCollection<Hotel>> GetAllAsync()
     {
-        return Task.FromResult<IReadOnlyCollection<Hotel>>(_hotels.AsReadOnly());
+        IReadOnlyCollection<Hotel> hotels = _hotels.Values.ToList();
+        return Task.FromResult(hotels);
     }
 
     public Task<bool> UpdateAsync(Hotel hotel)
     {
-        int index = _hotels.FindIndex(existingHotel => existingHotel.Id == hotel.Id);
-
-        if (index == -1)
+        if (!_hotels.ContainsKey(hotel.Id))
             return Task.FromResult(false);
 
-        _hotels[index] = hotel;
+        _hotels[hotel.Id] = hotel;
         return Task.FromResult(true);
     }
 
     public Task<bool> DeleteAsync(Guid id)
     {
-        Hotel? hotel = _hotels.FirstOrDefault(hotel => hotel.Id == id);
-
-        if (hotel is null)
-            return Task.FromResult(false);
-
-        _hotels.Remove(hotel);
-        return Task.FromResult(true);
+        return Task.FromResult(_hotels.TryRemove(id, out _));
     }
 }
